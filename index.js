243f0330b94c7f055646f1498f898b4cf15aaa9d
dimdejesus <ddejesus0320@gmail.com>
2023-09-05 18:46:27 +0800

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function toggleBulb() {
+    let imgBulb = document.getElementById('bulb')
+
+    if(imgBulb.src.includes('OFF'))
+        imgBulb.src = 'https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg'
+    else
+        imgBulb.src = 'https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg'
+}
